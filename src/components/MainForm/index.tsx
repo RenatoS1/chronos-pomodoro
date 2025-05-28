cb203f563944cfa91b083dd35400ad788b20ee9b
Renato Sena <rrenatosennacantuario@gmail.com>
2025-05-28 16:21:10 -0300

    Validando o formulário e criando uma nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,27 +2,65 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultInput } from "../DefaultInput";
 import { DefaultButton } from "../DefaultButton";
-import { useState } from "react";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('DEU CERTO', taskName);
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa !');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+
+      const secondsReamaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // Conferir
+        secondsReamaining,
+        formattedSecondsRemaining: '00:00',
+        task: [...prevState.tasks, newTask],
+
+      }
+    })
     
   }
 
   return (
-    <form onSubmit={handleCreateNewTask} className="form" action="">
+    <form onSubmit={handleCreateNewTask} 
+    className="form" action="">
       <div className="formRow">
         <DefaultInput
           id="meuInput"
           type="text"
           labelText="task:"
           placeholder="Digite Algo..."
-          value={taskName}
-          onChange={event => setTaskName(event.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -7,7 +7,14 @@ type TaskContextProviderProps = {
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
+
   const [state, setState] = useState(initialTaskState);
+
+  useEffect(() => {
+    console.log(state);
+    
+  }, [state])
+
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
