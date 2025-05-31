a6724bdd7086f53f62a761bb5c6c2eda2d782f31
Renato Sena <rrenatosennacantuario@gmail.com>
2025-05-31 16:44:26 -0300

    Entendendo useReducer, reducer, dispatch e actions

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -67,6 +67,7 @@ export function MainForm() {
           }
           return task;
         }),
+        
       };
     });
   }
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -10,15 +11,43 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
+  const [numero, dispatch] = useReducer((state, action) => {
+    console.log(state, action);
+
+
+    switch (action) {
+      case 'INCREMENT':
+        return state + 1;
+        break;
+
+      case 'DECREMENT':
+        return state - 1;
+        break;
+
+      case 'INITIAL_STATE':
+        return state = 0;
+        break;
+
+    }
     
-  }, [state])
+    return state;
+  }, 0)
+
+  // useEffect(() => {
+  //   console.log(state);
+    
+  // }, [state])
 
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>O número é : {numero}</h1>
+      <button type="button" onClick={() => dispatch('INCREMENT')}>
+        Incrementar</button>
+      <button type="button" onClick={() => dispatch('DECREMENT')}>
+        Decrementar</button>
+      <button type="button" onClick={() => dispatch('INITIAL_STATE')}>
+        Zerar</button>
     </TaskContext.Provider>
   );
 }
