90d1b98162d64b195de22e7015438521dde644da
Renato Sena <rrenatosennacantuario@gmail.com>
2025-06-24 14:09:40 -0300

    Tocando áudio ao completar uma tarefa (JavaScript Audio)

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,10 +1,11 @@
 //import { useEffect, useState } from "react";
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./TaskReducer";
 import { TaskActionsTypes } from "./taskActions";
 import { TimerWorkerManager } from "../../workers/TimeWorkerManager";
+import { loadBeep } from "../../utils/loadBeep";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,14 +13,25 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
+
+
 
   const worker = TimerWorkerManager.getInstance();
 
   worker.onmessage((e) => {
     const countDownSeconds = e.data;
-    console.log(countDownSeconds);
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        console.log('Tocando áudio...');
+        
+        playBeepRef.current();
+
+        playBeepRef.current = null;
+      }
+
+
       dispatch({
         type: TaskActionsTypes.COMPLETE_TASK,
       });
@@ -33,14 +45,28 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   });
 
   useEffect(() => {
-    console.log(state);
+    
     
     if (!state.activeTask) {
-      console.log("Worker terminado por falta de activeTask");
       worker.terminate();
     }
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      console.log('Carregando áudio...');
+      
+      playBeepRef.current = loadBeep();
+    } else {
+      console.log('Zerando áudio...');
+      playBeepRef.current = null;
+    }
+    
+  },[state.activeTask])
+
+
+
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
