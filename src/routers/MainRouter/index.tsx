78baf521e5c0721cd0a9ce0b009106d27f5ae5e2
Renato Sena <rrenatosennacantuario@gmail.com>
2025-07-08 18:03:20 -0300

    Formatando a data com date-fns

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -4,16 +4,15 @@ import { useEffect } from "react";
 import { Home } from "../../pages/Home";
 import { AboutPomodoro } from "../../pages/AboutPomodoro";
 import { NotFound } from "../../pages/NotFound";
-
-
+import { History } from "../../pages/History";
 
 function ScrollToTop() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, [pathname]);
-    return null;
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
 }
 
 export function MainRouter() {
@@ -21,8 +20,8 @@ export function MainRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/history/" element={<History />} />
         <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ScrollToTop />
